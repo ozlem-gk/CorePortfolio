8f5077af75ee406d13d27fdf76a1eb66e1483112
ozlem goksu <ozlemgoksu4@gmail.com>
2022-09-01 15:29:25 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IAboutDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IAboutDal
+    {
+        void Insert();
+        void Update();  
+        void Delete();
+        List<About> GetList();
+    }
+}
---
+++ EntityLayer/Concreate/Education.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class Education
+    {
+        [Key]
+        public int EducationID { get; set; }
+        public string MajorName { get; set; }
+        public string School { get; set; }
+        public string StartDate { get; set; }
+        public string FinishDate { get; set; }
+        public string Grade { get; set; }
+    }
+}
