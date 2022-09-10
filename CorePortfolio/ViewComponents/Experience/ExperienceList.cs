a0a37342b071d9dc90087f143578c176ce16359e
ozlem goksu <ozlemgoksu4@gmail.com>
2022-09-10 10:26:49 +0300

    !

---
+++ CorePortfolio/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.ViewComponents.Experience
+{
+    
+    public class ExperienceList:ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}
--- CorePortfolio/ViewComponents/Portfolio/PortfolioList.cs
+++ CorePortfolio/ViewComponents/Portfolio/PortfolioList.cs
@@ -1,14 +1,16 @@
 ﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CorePortfolio.ViewComponents.Portfolio
 {
     public class PortfolioList:ViewComponent
     {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
         public IViewComponentResult Invoke()
         {
-            
-            return View();
+            var values= portfolioManager.TGetList();    
+            return View(values);
         }
     }
 }
