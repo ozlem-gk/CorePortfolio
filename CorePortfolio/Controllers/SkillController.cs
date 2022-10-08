75ba95583d8b819b3a4f50d23de1849416db87c0
ozlem goksu <ozlemgoksu4@gmail.com>
2022-10-08 23:34:05 +0300

    Skills completed.

---
+++ CorePortfolio/Controllers/SkillController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillmanager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
+            var values = skillmanager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillmanager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillmanager.TGetByID(id);
+            skillmanager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSkill(int id)
+        {
+            var values = skillmanager.TGetByID(id);
+            skillmanager.TUpdate(values);
+            return RedirectToAction("Index");
+        }
+    }
+}
