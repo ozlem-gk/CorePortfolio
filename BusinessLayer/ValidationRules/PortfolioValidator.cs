293143440659316ee1c62c9290e839bd65e422d7
ozlem goksu <ozlemgoksu4@gmail.com>
2022-10-14 02:10:29 +0300

    Added Fluent Validation

---
+++ BusinessLayer/ValidationRules/PortfolioValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concreate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class PortfolioValidator:AbstractValidator<Portfolio>
+    {
+        public PortfolioValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Proje Adı boş geçilemez");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel alanı boş geçilemez");
+            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Görsel2 alanı boş geçilemez");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat alanı boş geçilemez");
+            RuleFor(x => x.value).NotEmpty().WithMessage("Değer alanı boş geçilemez");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Proje adı en az 5 karakter olmak zorundadır");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("Proje adı en fazla 100 karakter olmak zorundadır");
+        }
+    }
+}
--- CorePortfolio/Controllers/PortfolioController.cs
+++ CorePortfolio/Controllers/PortfolioController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concreate;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concreate;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ValidationResult = FluentValidation.Results.ValidationResult;
 
 namespace CorePortfolio.Controllers
 {
@@ -20,16 +24,31 @@ namespace CorePortfolio.Controllers
         [HttpGet]
         public IActionResult AddPortfolio()
         {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Ekleme";
             return View();  
         }
         [HttpPost]
         public IActionResult AddPortfolio(Portfolio portfolio)
         {
-            ViewBag.v1 = "Proje Listesi";
-            ViewBag.v2 = "Projeler";
-            ViewBag.v3 = "Proje Ekleme";
-            portfolioManager.TAdd(portfolio);
-            return RedirectToAction("Index"); 
+            
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult result = validations.Validate(portfolio);  
+            if (result.IsValid)
+            {
+                portfolioManager.TAdd(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+          
         }
 
     }
