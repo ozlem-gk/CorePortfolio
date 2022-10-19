8150c8574b1a659b2984824659f5f1a6b88ce0b7
ozlem goksu <ozlemgoksu4@gmail.com>
2022-10-20 00:17:54 +0300

    !

---
+++ CorePortfolio/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x=>x.Status==false).Count();
+            ViewBag.v3 = c.Messages.Where(x=>x.Status==true).Count();
+            ViewBag.v4 = c.Experiences.Count();
+
+            //var values = contactManager.TGetList();
+            return View();
+        }
+    }
+}
---
+++ CorePortfolio/ViewComponents/Dashboard/Last5Projects.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.ViewComponents.Dashboard
+{
+    public class Last5Projects : ViewComponent
+    {
+       
+        public IViewComponentResult Invoke()
+        {
+           ;
+            return View();
+        }
+    }
+    
+}
