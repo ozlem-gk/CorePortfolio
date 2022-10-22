cbb7651fb8eb56b6a08d23e37a45cc50e4c56bd0
ozlem goksu <ozlemgoksu4@gmail.com>
2022-10-22 23:31:31 +0300

    Have added Area and another Layout

--- CorePortfolio/Program.cs
+++ CorePortfolio/Program.cs
@@ -24,4 +24,14 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
+
+
---
+++ CorePortfolio/ViewComponents/Dashboard/ToDoListPanel.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.ViewComponents.Dashboard
+{
+    public class ToDoListPanel : ViewComponent
+    {
+        ToDoListManager _toDoListManager = new ToDoListManager(new EfToDoListDal());
+       public IViewComponentResult Invoke()
+        {
+            var values = _toDoListManager.TGetList();
+            return View(values);
+        }
+    }
+}
+
